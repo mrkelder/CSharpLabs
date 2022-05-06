a2c2a428040cfc7ac765d4f206e92a03c177de98
Vlad A <vageev06@gmail.com>
2022-05-06 15:37:13 +0300

    lab1

--- Program.cs
+++ Program.cs
@@ -5,9 +5,49 @@ namespace ConsoleApp1
 
     class Car
     {
-        public readonly string vin = "82758";
         public readonly string numberPlate = "DC2412R";
+        private int gear = 1;
         private int fuelLeft = 15;
+
+        public void changeGear (char gear)
+        {
+            if (gear == 'r' || gear == 'R') this.gear = -1;
+        }
+
+        public void changeGear(int gear)
+        {
+            if (gear >= 0 && gear <= 5) this.gear = Convert.ToChar(gear);
+        }
+
+        public void driveOneMile()
+        {
+            if (this.fuelLeft > 0) this.fuelLeft--;
+            else Console.WriteLine("Бак пуст");
+        }
+
+        public void printStats()
+        {
+            this.printCurrentGear();
+            this.printFuelStatus();
+            this.printNumberPlate();
+            Console.WriteLine();
+        }  
+
+        private void printNumberPlate()
+        {
+            Console.WriteLine("Номер авто: " + this.numberPlate);
+        }
+
+        private void printFuelStatus()
+        {
+            if (this.fuelLeft > 0) Console.WriteLine("Осталось " + this.fuelLeft +" единиц топлива");
+            else Console.WriteLine("Бак пуст");
+        }
+        private void printCurrentGear()
+        {
+            if (this.gear == -1) Console.WriteLine("Задняя передача");
+            else Console.WriteLine("Данная передача: " + this.gear);
+        }
 
         public string FuelLeft
         {
@@ -35,11 +75,20 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Car bmw = new Car();
-            Console.WriteLine(bmw.FuelLeft);
-            bmw.FuelLeft = "40";
-            Console.WriteLine(bmw.FuelLeft);
+            bmw.printStats();
 
-            // Console.ReadKey();
+            bmw.FuelLeft = "2";
+            bmw.changeGear(5);
+            bmw.printStats();
+
+            bmw.driveOneMile();
+            bmw.changeGear('R');
+            bmw.printStats();
+
+
+            bmw.driveOneMile();
+            bmw.changeGear(1);
+            bmw.printStats();
         }
     }
 }
