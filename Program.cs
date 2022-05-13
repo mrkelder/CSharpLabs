038b2142c083ef8bf790f23369bc249b6d593504
Vlad A <vageev06@gmail.com>
2022-05-13 15:31:20 +0300

    lab4

--- Program.cs
+++ Program.cs
@@ -3,100 +3,100 @@
 namespace ConsoleApp1
 {
 
-    class MyMath
+    public interface Descriminant {
+        public float A { get; set; }
+        public float B { get; set; }
+        public float C { get; set; }
+        public float findDescrimenant();
+    }
+
+    public interface SquareRoots
     {
-        public virtual void square(int num)
-        {
-            Console.WriteLine("From MyMath");
-            Console.WriteLine(num * num);
-            Console.WriteLine();
+        public float A { get; set; }
+        public float B { get; set; }
+        public float C { get; set; }
+        public void printSquareRoots();
+    }
 
+    class DescrimenantFinder : Descriminant {
+        private float a;
+        private float b;
+        private float c;
+
+        public virtual float A
+        {
+            set { a = value; }
+            get { return a; }
         }
 
-        public void add20ToInitialNumber(ref int num)
+        public virtual float B
         {
-            num += 20;
+            set { b = value; }
+            get { return b; }
+        }
+        public virtual float C
+        {
+            set { c = value; }
+            get { return c; }
         }
 
-        public void add20ToInitialNumber(ref int num, params int[] nums)
+        public float findDescrimenant()
         {
-            bool fromMinToMax = num % 2 == 0;
-            int[] newArray = nums;
+            return MathF.Pow(b, 2) - 4 * a * c;
+        }
+    }
 
-            for (var a = 0; a < newArray.Length - 1; a++)
+    class SquareRootsFinder: DescrimenantFinder, SquareRoots {
+        private float a;
+        private float b;
+        private float c;
+
+        public override float A
+        {
+            set { base.A = value; a = value; }
+            get { return a; }
+        }
+
+        public override float B
+        {
+            set { base.B = value;  b = value; }
+            get { return b; }
+        }
+        public override float C
+        {
+            set { base.C = value; c = value; }
+            get { return c; }
+        }
+
+        public void printSquareRoots ()
+        {
+            float D = this.findDescrimenant();
+
+            if (D > 0)
             {
-                for(var b = 0; b < newArray.Length - a; b++)
-                {
-                    if(fromMinToMax)
-                    {
-                        if (newArray[a + b] < newArray[a])
-                        {
-                            int temp = newArray[a + b];
-                            newArray[a + b] = newArray[a];
-                            newArray[a] = temp;
-                        }
-                    }
-
-                    else
-                    {
-                        if (newArray[a + b] > newArray[a])
-                        {
-                            int temp = newArray[a + b];
-                            newArray[a + b] = newArray[a];
-                            newArray[a] = temp;
-                        }
-                    }                    
-                }
+                Console.WriteLine("x1 = {0}", (MathF.Pow(b, 2) + Math.Sqrt(D)) / 2 * a);
+                Console.WriteLine("x2 = {0}", (MathF.Pow(b, 2) - Math.Sqrt(D)) / 2 * a);
             }
+            else if (D == 0)
+                Console.WriteLine("x = {0}", (MathF.Pow(b, 2) + Math.Sqrt(D)) / 2 * a);
+            else
+                Console.WriteLine("Корней нет");
 
-            Console.Write("[");
-
-            foreach(var i in newArray)
-            {
-                Console.Write(" {0} ", i);
-            }
-            Console.WriteLine("]");
         }
     }
 
-    class MyMath2: MyMath
-    {
-        public override void square(int num)
-        {
-            Console.WriteLine("From MyMath2");
-            Console.WriteLine(num * num * num);
-            base.square(num);
-        }
-    }
-
-    class MyMath3 : MyMath2
-    {
-        public override void square(int num)
-        {
-            Console.WriteLine("From MyMath3");
-            Console.WriteLine(num * num * num * num);
-            base.square(num);
-        }
-    }
 
     class Program
     {
         static void Main(string[] args)
         {
+            SquareRootsFinder srf = new SquareRootsFinder();
 
-            MyMath ex = new MyMath();
-            MyMath2 ex2 = new MyMath2();
-            MyMath3 ex3 = new MyMath3();
-
-            int num = 2;
-
-
-            ex.square(2);
-            ex.add20ToInitialNumber(ref num);
-            Console.WriteLine(num);
-            ex.add20ToInitialNumber(ref num, 1, 2 ,3 ,1, 5 ,8 ,7, 1);
-            ex2.square(3);
-            ex3.square(5);
+            srf.A = 3;
+            srf.B = 2;
+            srf.C = -1;
+             
+            srf.printSquareRoots();
         }
     }
 }
